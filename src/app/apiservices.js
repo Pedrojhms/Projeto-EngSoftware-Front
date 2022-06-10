dbaa55064e0d625acf4ea9f320b56ec19e4ca4a2
pedrojhms <j.pedrohenrique@cc.ci.ufpb.br>
2022-06-10 16:51:03 -0300

    Configuração Heroku

--- src/app/apiservices.js
+++ src/app/apiservices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://projetofinalraoni-backend.herokuapp.com/",
 });
 
 class ApiService {
