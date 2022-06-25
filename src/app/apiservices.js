601d47c42cf83967cc61440e6b9d4cbb3e55a376
pedrojhms <j.pedrohenrique2011@gmail.com>
2022-06-25 19:27:23 -0300

    Hotfix

--- src/app/apiservices.js
+++ src/app/apiservices.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  //baseURL: "https://projetofinalraoni-backend.herokuapp.com/",
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://projetofinalraoni-backend.herokuapp.com/",
+  //baseURL: "http://localhost:8080/",
   withCredentials: true,
 });
 
